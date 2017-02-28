2b9288904639ff90acc704d22aa0d9c7eaabdf9a
Vincent Ragosta <vragosta@storycorps.org>
2017-02-28 10:26:23 -0500

    modified CSS, added social icons

---
+++ themes/twenty-seventeen/partials/content-social.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Studio Wall Social Icons
+ *
+ * @package    Studio Tag - Twenty Seventeen
+ * @subpackage Studio Wall - Twenty Seventeen
+ * @since      0.1.0
+ */
+?>
+
+<div class="social">
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/twitter.png' ?>" /></a>
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/facebook.png' ?>" /></a>
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/pinterest.png' ?>" /></a>
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/twitter.png' ?>" /></a>
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/instagram.png' ?>" /></a>
+	<a href="#"><img src="<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/linkedin.png' ?>" /></a>
+</div>
