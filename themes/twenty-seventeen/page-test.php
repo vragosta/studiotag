86a09e1517a6bb7077cb4dd0f0e96c0cab1b43d8
Vincent Ragosta <vragosta@storycorps.org>
2017-03-07 11:28:43 -0500

    forgot to add new files

---
+++ themes/twenty-seventeen/page-test.php
@@ -0,0 +1,120 @@
+<?php get_header(); ?>
+
+<!-- <section style="height: 1000px; display: flex; justify-content: center; align-items: center;">Placeholder</section> -->
+<!-- This will be a carousel -->
+<!-- main image in a tag-->
+<section id="banner-image">
+			<figure>
+					<a></a>
+			</figure>
+	</section>
+
+	<!-- Wall Systems image -->
+	<main id="content">
+		<section>
+			<figure>
+				<a></a>
+			</figure>
+		</section>
+
+		<!-- list of images with  -->
+		<ul class="img-grid">
+			<li>
+				<!-- left blank intentionally -->
+			</li>
+			<li>
+				<img src="img/Categories_Images/01_encore.jpg">
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<img src="img/Categories_Images/02_slimline.jpg">
+			</li>
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Encore</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Slimline</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+			<li>
+				<img src="img/Categories_Images/03_ion.jpg">
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<img src="img/Categories_Images/04_industrial-sash.jpg">
+			</li>
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Ion</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Industrial Sash</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+			<li>
+				<img src="img/Categories_Images/05_greenwall.png">
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<img src="img/Categories_Images/06_prebuilt.jpg">
+			</li>
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Greenwall</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+		</ul>
+
+		<ul class="img-grid">
+			<li>
+				<span>Tagwall</span>
+				<a href="#">Prebuilt</a>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+									in tellus id lectus imperdiet hendrerit. Curabitur molestie faucibus
+									orci, bibendum elementum ex sagittis non.
+							</p>
+			</li>
+		</ul>
+
+	</main>
+
+
+<?php get_footer(); ?>
