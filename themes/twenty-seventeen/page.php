15369a00485484f1363707be9bc72abd2732adb6
Vincent Ragosta <vragosta@storycorps.org>
2017-03-08 11:29:32 -0500

    added images, began reconstructing the template

--- themes/twenty-seventeen/page.php
+++ themes/twenty-seventeen/page.php
@@ -1,13 +1,83 @@
 <?php get_header(); ?>
 
-	<section style="height: 500px; display: flex; justify-content: center; align-items: center; border: 0.1rem solid #333; margin: 2rem 1rem;">
-		Slider Placeholder
-	</section>
+	<main>
+		<section style="height: 900px; display: flex; justify-content: center; align-items: center; border: 0.1rem solid #333; margin: 2rem 0;">
+			Slider Placeholder
+		</section>
 
-	<section style="margin: 2rem 1rem;">
-		<figure style="padding-bottom: 56.25%; position: relative; width: 65%;">
-			<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/wallsystems.png'; ?>' ); background-size: contain; position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></div>
-		</figure>
-	</section>
+		<section style="margin: 2rem 1rem -8rem;">
+			<figure style="padding-bottom: 40%; position: relative; width: 85%;">
+				<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/wallsystems.png'; ?>' ); background-size: contain; position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></div>
+			</figure>
+		</section>
+
+		<section style="margin: 0 0 40rem 0;">
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-offset-6 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/encore.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
+					</figure>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;" style="padding: 0;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/slimline.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
+					</figure>
+				</div>
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 10rem 1rem 4rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Encore</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+
+				</div>
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/ion.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
+					</figure>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;" style="padding: 0;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/industrialsash.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
+					</figure>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+
+				</div>
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/greenwall.png'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
+					</figure>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 0;">
+					<figure style="padding-bottom: 70%; position: relative;" style="padding: 0;">
+						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/prebuilt.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
+					</figure>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+
+				</div>
+			</div>
+
+		</section>
+	</main>
 
 <?php get_footer(); ?>
