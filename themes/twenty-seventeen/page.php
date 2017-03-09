bef573d0fc65b55882589959b30dc03e2132d23a
Vincent Ragosta <allchenzo@gmail.com>
2017-03-08 19:48:20 -0500

    added temporary slider placeholder image, finished templating initial pass over this template

--- themes/twenty-seventeen/page.php
+++ themes/twenty-seventeen/page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
 	<main>
-		<section style="height: 900px; display: flex; justify-content: center; align-items: center; border: 0.1rem solid #333; margin: 2rem 0;">
-			Slider Placeholder
-		</section>
+		<!-- NOTE: will replace with slider -->
+		<figure style="padding-bottom: 68%; position: relative;">
+			<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/vigoss.jpg'; ?>' ); background-size: contain; position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></div>
+		</figure>
 
 		<section style="margin: 2rem 1rem -8rem;">
 			<figure style="padding-bottom: 40%; position: relative; width: 85%;">
@@ -34,8 +35,9 @@
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12 col-sm-6" style="padding: 0;">
-
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 4rem 1rem 14rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Slimline</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
 				</div>
 				<div class="col-xs-12 col-sm-6" style="padding: 0;">
 					<figure style="padding-bottom: 70%; position: relative;">
@@ -50,14 +52,16 @@
 						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/industrialsash.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
 					</figure>
 				</div>
-				<div class="col-xs-12 col-sm-6">
-
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 10rem 1rem 4rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Ion</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12 col-sm-6" style="padding: 0;">
-
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 4rem 1rem 14rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Industrial Sash</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
 				</div>
 				<div class="col-xs-12 col-sm-6" style="padding: 0;">
 					<figure style="padding-bottom: 70%; position: relative;">
@@ -72,8 +76,16 @@
 						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/prebuilt.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
 					</figure>
 				</div>
-				<div class="col-xs-12 col-sm-6">
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 10rem 1rem 4rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Greenwall</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
+				</div>
+			</div>
 
+			<div class="row">
+				<div class="col-xs-12 col-sm-6" style="padding: 1rem 4rem 1rem 14rem;">
+					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Prebuilt</h3>
+					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
 				</div>
 			</div>
 
