a1c0619e0257f8685e612c3672a6c7cdd16b1674
Vincent Ragosta <allchenzo@gmail.com>
2017-03-09 00:45:34 -0500

    created system custom post type, created WP loop to cycle through all systems and execute the appropriate HTML

--- mu-plugins/custom-post-type-taxonomy.php
+++ mu-plugins/custom-post-type-taxonomy.php
@@ -1,10 +1,9 @@
 <?php
 // TODO
 
-/* Company Post Type */
-function register_post_type_company() {
-	register_post_type( 'company', array(
-			'label' => 'Company',
+function register_post_type_system() {
+	register_post_type( 'system', array(
+			'label' => 'System',
 			'description' => '',
 			'public' => true,
 			'show_ui' => true,
@@ -12,7 +11,7 @@ function register_post_type_company() {
 			'capability_type' => 'post',
 			'map_meta_cap' => true,
 			'hierarchical' => false,
-			'rewrite' => array( 'slug' => 'companies', 'with_front' => false ),
+			'rewrite' => array( 'slug' => 'systems', 'with_front' => false ),
 			'query_var' => true,
 			'has_archive' => true,
 			'menu_position' => 41,
@@ -20,61 +19,100 @@ function register_post_type_company() {
 			'taxonomies'  => array( 'category' ),
 			'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'revisions', 'thumbnail', 'author' ),
 			'labels' => array (
-					'name' => 'Companies',
-					'singular_name' => 'Company',
-					'menu_name' => 'Companies',
-					'add_new' => 'Add Company',
-					'add_new_item' => 'Add New Company',
+					'name' => 'Systems',
+					'singular_name' => 'System',
+					'menu_name' => 'Systems',
+					'add_new' => 'Add System',
+					'add_new_item' => 'Add New System',
 					'edit' => 'Edit',
-					'edit_item' => 'Edit Company',
-					'new_item' => 'New Company',
-					'view' => 'View Company',
-					'view_item' => 'View Company',
-					'search_items' => 'Search Companies',
-					'not_found' => 'No Companies Found',
-					'not_found_in_trash' => 'No Companies Found in Trash',
-					'parent' => 'Parent Company',
+					'edit_item' => 'Edit System',
+					'new_item' => 'New System',
+					'view' => 'View System',
+					'view_item' => 'View System',
+					'search_items' => 'Search Systems',
+					'not_found' => 'No Systems Found',
+					'not_found_in_trash' => 'No Systems Found in Trash',
+					'parent' => 'Parent System',
 			)
 		)
 	);
 }
-add_action( 'init', 'register_post_type_company' );
+add_action( 'init', 'register_post_type_system' );
 
-function register_post_type_manufacturer() {
-	register_post_type( 'manufacturer', array(
-			'label' => 'Manufacturer',
-			'description' => '',
-			'public' => true,
-			'show_ui' => true,
-			'show_in_menu' => true,
-			'capability_type' => 'post',
-			'map_meta_cap' => true,
-			'hierarchical' => false,
-			'rewrite' => array( 'slug' => 'manufacturers', 'with_front' => false ),
-			'query_var' => true,
-			'has_archive' => true,
-			'menu_position' => 41,
-			'menu_icon' => 'dashicons-feedback',
-			'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'revisions', 'thumbnail', 'author', 'category' ),
-			'labels' => array (
-					'name' => 'Manufacturers',
-					'singular_name' => 'Manufacturer',
-					'menu_name' => 'Manufacturers',
-					'add_new' => 'Add Manufacturer',
-					'add_new_item' => 'Add New Manufacturer',
-					'edit' => 'Edit',
-					'edit_item' => 'Edit Manufacturer',
-					'new_item' => 'New Manufacturer',
-					'view' => 'View Manufacturer',
-					'view_item' => 'View Manufacturer',
-					'search_items' => 'Search Manufacturers',
-					'not_found' => 'No Manufacturers Found',
-					'not_found_in_trash' => 'No Manufacturers Found in Trash',
-					'parent' => 'Parent Manufacturer',
-			)
-		)
-	);
-}
-add_action( 'init', 'register_post_type_manufacturer' );
+/* Company Post Type */
+// function register_post_type_company() {
+// 	register_post_type( 'company', array(
+// 			'label' => 'Company',
+// 			'description' => '',
+// 			'public' => true,
+// 			'show_ui' => true,
+// 			'show_in_menu' => true,
+// 			'capability_type' => 'post',
+// 			'map_meta_cap' => true,
+// 			'hierarchical' => false,
+// 			'rewrite' => array( 'slug' => 'companies', 'with_front' => false ),
+// 			'query_var' => true,
+// 			'has_archive' => true,
+// 			'menu_position' => 41,
+// 			'menu_icon' => 'dashicons-feedback',
+// 			'taxonomies'  => array( 'category' ),
+// 			'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'revisions', 'thumbnail', 'author' ),
+// 			'labels' => array (
+// 					'name' => 'Companies',
+// 					'singular_name' => 'Company',
+// 					'menu_name' => 'Companies',
+// 					'add_new' => 'Add Company',
+// 					'add_new_item' => 'Add New Company',
+// 					'edit' => 'Edit',
+// 					'edit_item' => 'Edit Company',
+// 					'new_item' => 'New Company',
+// 					'view' => 'View Company',
+// 					'view_item' => 'View Company',
+// 					'search_items' => 'Search Companies',
+// 					'not_found' => 'No Companies Found',
+// 					'not_found_in_trash' => 'No Companies Found in Trash',
+// 					'parent' => 'Parent Company',
+// 			)
+// 		)
+// 	);
+// }
+// add_action( 'init', 'register_post_type_company' );
+
+// function register_post_type_manufacturer() {
+// 	register_post_type( 'manufacturer', array(
+// 			'label' => 'Manufacturer',
+// 			'description' => '',
+// 			'public' => true,
+// 			'show_ui' => true,
+// 			'show_in_menu' => true,
+// 			'capability_type' => 'post',
+// 			'map_meta_cap' => true,
+// 			'hierarchical' => false,
+// 			'rewrite' => array( 'slug' => 'manufacturers', 'with_front' => false ),
+// 			'query_var' => true,
+// 			'has_archive' => true,
+// 			'menu_position' => 41,
+// 			'menu_icon' => 'dashicons-feedback',
+// 			'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'revisions', 'thumbnail', 'author', 'category' ),
+// 			'labels' => array (
+// 					'name' => 'Manufacturers',
+// 					'singular_name' => 'Manufacturer',
+// 					'menu_name' => 'Manufacturers',
+// 					'add_new' => 'Add Manufacturer',
+// 					'add_new_item' => 'Add New Manufacturer',
+// 					'edit' => 'Edit',
+// 					'edit_item' => 'Edit Manufacturer',
+// 					'new_item' => 'New Manufacturer',
+// 					'view' => 'View Manufacturer',
+// 					'view_item' => 'View Manufacturer',
+// 					'search_items' => 'Search Manufacturers',
+// 					'not_found' => 'No Manufacturers Found',
+// 					'not_found_in_trash' => 'No Manufacturers Found in Trash',
+// 					'parent' => 'Parent Manufacturer',
+// 			)
+// 		)
+// 	);
+// }
+// add_action( 'init', 'register_post_type_manufacturer' );
 
 ?>
--- themes/twenty-seventeen/page.php
+++ themes/twenty-seventeen/page.php
@@ -1,10 +1,27 @@
-<?php get_header(); ?>
+<?php
+	// TODO
+
+	get_header();
+
+	// TODO
+	$count = 0;
+
+	// TODO
+	$args = array(
+		'post_type' => array( 'system' ),
+		'order'     => 'ASC'
+	);
+
+	// TODO
+	$query = new WP_Query( $args );
+?>
 
 	<main>
 		<!-- NOTE: will replace with slider -->
 		<figure style="padding-bottom: 68%; position: relative;">
 			<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/vigoss.jpg'; ?>' ); background-size: contain; position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></div>
 		</figure>
+		<!-- END -->
 
 		<section style="margin: 2rem 1rem -8rem;">
 			<figure style="padding-bottom: 40%; position: relative; width: 85%;">
@@ -14,7 +31,62 @@
 
 		<section style="margin: 0 0 40rem 0;">
 
-			<div class="row">
+			<?php if ( $query->have_posts() ) : ?>
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+					<!-- TODO Create custom image size. -->
+					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0]; ?>
+
+				<?php //elseif ( ( $query->current_post ) == ( $query->post_count ) ) : ?>
+					<!-- <div class="row">
+						<div class="col-xs-12 col-sm-6" style="padding: 1rem 4rem 1rem 14rem;">
+							<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> <?php echo esc_html( $temp_post->post_title ); ?></h3>
+							<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;"><?php echo esc_html( $temp_post->post_excerpt ); ?></p>
+						</div>
+					</div> -->
+
+					<?php if ( $count == 0 ) : ?>
+						<div class="row">
+							<div class="col-xs-12 col-sm-offset-6 col-sm-6" style="padding: 0;">
+								<figure style="padding-bottom: 70%; position: relative;">
+									<div style="background-image: url( '<?php echo esc_attr( $image ); ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
+								</figure>
+							</div>
+						</div>
+					<?php elseif ( $count % 2 == 1 ) : ?>
+						<div class="row">
+							<div class="col-xs-12 col-sm-6" style="padding: 0;">
+								<figure style="padding-bottom: 70%; position: relative;" style="padding: 0;">
+									<div style="background-image: url( '<?php echo esc_attr( $image ); ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 10rem; right: 0;"></div>
+								</figure>
+							</div>
+							<div class="col-xs-12 col-sm-6" style="padding: 1rem 10rem 1rem 4rem;">
+								<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> <?php echo esc_html( $temp_post->post_title ); ?></h3>
+								<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;"><?php echo esc_html( $temp_post->post_excerpt ); ?></p>
+							</div>
+						</div>
+					<?php elseif ( $count % 2 == 0 ) : ?>
+						<div class="row">
+							<div class="col-xs-12 col-sm-6" style="padding: 1rem 4rem 1rem 14rem;">
+								<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> <?php echo esc_html( $temp_post->post_title ); ?></h3>
+								<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;"><?php echo esc_html( $temp_post->post_excerpt ); ?></p>
+							</div>
+							<div class="col-xs-12 col-sm-6" style="padding: 0;">
+								<figure style="padding-bottom: 70%; position: relative;">
+									<div style="background-image: url( '<?php echo esc_attr( $image ); ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
+								</figure>
+							</div>
+						</div>
+					<?php endif; ?>
+
+					<!-- TODO -->
+					<?php $temp_post = $post; ?>
+					<?php $count++; ?>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+
+			<!-- <div class="row">
 				<div class="col-xs-12 col-sm-offset-6 col-sm-6" style="padding: 0;">
 					<figure style="padding-bottom: 70%; position: relative;">
 						<div style="background-image: url( '<?php echo STUDIO_WALL_TEMPLATE_URL . '/assets/images/encore.jpg'; ?>' ); background-size: cover; position: absolute; top: 0; bottom: 0; left: 0; right: 10rem;"></div>
@@ -87,7 +159,7 @@
 					<h3 style="font-size: 3.5rem; font-weight: 600; margin-bottom: 10rem;"><span style="font-family: HelveticaNeueLT Std; color: #A3A5A8;">Tagwall</span> Prebuilt</h3>
 					<p style="font-family: HelveticaNeueLT Std; color: #A3A5A8; font-size: 2.8rem; line-height: 1.3;">Comfortable conversation or quiet contemplation? Find furniture to create a room that will be the epicenter of your home.</p>
 				</div>
-			</div>
+			</div> -->
 
 		</section>
 	</main>
