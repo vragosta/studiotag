d5aef29eebf5b557a41051dd4b2624e52d99f75d
Vincent Ragosta <allchenzo@gmail.com>
2017-03-12 17:52:54 -0400

    resolved all MQ for systems single template

--- themes/twenty-seventeen/single-system.php
+++ themes/twenty-seventeen/single-system.php
@@ -15,27 +15,27 @@
 		<?php while( have_posts() ) : the_post(); ?>
 
 			<?php
-				// TODO
-				$custom_one_title      = json_decode( get_post_meta( $post->ID, 'custom_one_title', true ) );
-				$custom_one_content    = json_decode( get_post_meta( $post->ID, 'custom_one_content', true ) );
-				$custom_two_title      = json_decode( get_post_meta( $post->ID, 'custom_two_title', true ) );
-				$custom_two_content    = json_decode( get_post_meta( $post->ID, 'custom_two_content', true ) );
-				$custom_three_title    = json_decode( get_post_meta( $post->ID, 'custom_three_title', true ) );
-				$custom_three_content  = json_decode( get_post_meta( $post->ID, 'custom_three_content', true ) );
-				$custom_four_title     = json_decode( get_post_meta( $post->ID, 'custom_four_title', true ) );
-				$custom_four_content   = json_decode( get_post_meta( $post->ID, 'custom_four_content', true ) );
-				$custom_five_title     = json_decode( get_post_meta( $post->ID, 'custom_five_title', true ) );
-				$custom_five_content   = json_decode( get_post_meta( $post->ID, 'custom_five_content', true ) );
-				$custom_six_title      = json_decode( get_post_meta( $post->ID, 'custom_six_title', true ) );
-				$custom_six_content    = json_decode( get_post_meta( $post->ID, 'custom_six_content', true ) );
-				$custom_seven_title    = json_decode( get_post_meta( $post->ID, 'custom_seven_title', true ) );
-				$custom_seven_content  = json_decode( get_post_meta( $post->ID, 'custom_seven_content', true ) );
-				$custom_eight_title    = json_decode( get_post_meta( $post->ID, 'custom_eight_title', true ) );
-				$custom_eight_content  = json_decode( get_post_meta( $post->ID, 'custom_eight_content', true ) );
-				$custom_nine_title     = json_decode( get_post_meta( $post->ID, 'custom_nine_title', true ) );
-				$custom_nine_content   = json_decode( get_post_meta( $post->ID, 'custom_nine_content', true ) );
-				$custom_ten_title      = json_decode( get_post_meta( $post->ID, 'custom_ten_title', true ) );
-				$custom_ten_content    = json_decode( get_post_meta( $post->ID, 'custom_ten_content', true ) );
+				// Get all 'system' specific meta.
+				$custom_one_title     = json_decode( get_post_meta( $post->ID, 'custom_one_title', true ) );
+				$custom_one_content   = json_decode( get_post_meta( $post->ID, 'custom_one_content', true ) );
+				$custom_two_title     = json_decode( get_post_meta( $post->ID, 'custom_two_title', true ) );
+				$custom_two_content   = json_decode( get_post_meta( $post->ID, 'custom_two_content', true ) );
+				$custom_three_title   = json_decode( get_post_meta( $post->ID, 'custom_three_title', true ) );
+				$custom_three_content = json_decode( get_post_meta( $post->ID, 'custom_three_content', true ) );
+				$custom_four_title    = json_decode( get_post_meta( $post->ID, 'custom_four_title', true ) );
+				$custom_four_content  = json_decode( get_post_meta( $post->ID, 'custom_four_content', true ) );
+				$custom_five_title    = json_decode( get_post_meta( $post->ID, 'custom_five_title', true ) );
+				$custom_five_content  = json_decode( get_post_meta( $post->ID, 'custom_five_content', true ) );
+				$custom_six_title     = json_decode( get_post_meta( $post->ID, 'custom_six_title', true ) );
+				$custom_six_content   = json_decode( get_post_meta( $post->ID, 'custom_six_content', true ) );
+				$custom_seven_title   = json_decode( get_post_meta( $post->ID, 'custom_seven_title', true ) );
+				$custom_seven_content = json_decode( get_post_meta( $post->ID, 'custom_seven_content', true ) );
+				$custom_eight_title   = json_decode( get_post_meta( $post->ID, 'custom_eight_title', true ) );
+				$custom_eight_content = json_decode( get_post_meta( $post->ID, 'custom_eight_content', true ) );
+				$custom_nine_title    = json_decode( get_post_meta( $post->ID, 'custom_nine_title', true ) );
+				$custom_nine_content  = json_decode( get_post_meta( $post->ID, 'custom_nine_content', true ) );
+				$custom_ten_title     = json_decode( get_post_meta( $post->ID, 'custom_ten_title', true ) );
+				$custom_ten_content   = json_decode( get_post_meta( $post->ID, 'custom_ten_content', true ) );
 			?>
 
 			<figure class="featured-image">
@@ -43,14 +43,14 @@
 			</figure>
 
 			<section>
-				<div class="row">
+				<div class="title-container row">
 					<div class="col-xs-12">
 						<h1><span>Tagwall</span> Encore</h1>
 					</div>
 				</div>
 
-				<div class="content-container row">
-					<div class="col-xs-12 col-sm-3">
+				<div class="specs-container row">
+					<div class="view-container col-xs-12 col-sm-5 col-md-3 col-lg-3">
 						<ul>
 							<li><a href="#">View Gallery</a></li>
 							<li><a href="#">View Wall Details</a></li>
@@ -58,11 +58,11 @@
 						</ul>
 					</div>
 
-					<div class="col-xs-12 col-sm-4">
+					<div class="content-container col-xs-12 col-sm-7 col-md-5 col-lg-4">
 						<?php the_content(); ?>
 					</div>
 
-					<div class="col-xs-12 col-sm-5">
+					<div class="custom-meta-container col-xs-12 col-sm-12 col-md-4 col-lg-5">
 
 						<?php if ( $custom_one_title ) : ?>
 							<div class="custom one">
