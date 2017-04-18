908b388c37b30008666373745591f31390298d01
Vincent Ragosta <allchenzo@gmail.com>
2017-04-18 19:08:13 -0400

    completely reworked hamburger menu

--- themes/twenty-seventeen/assets/js/tagwall---twenty-seventeen.js
+++ themes/twenty-seventeen/assets/js/tagwall---twenty-seventeen.js
@@ -9,36 +9,95 @@
 ( function( $ ) {
 	$( document ).ready(function() {
 
+	$( '.header-navigation-front-page .hamburger' ).on( 'click', function() {
+
+		// Toggle hamburger button appearance.
+		$( '.hamburger' )
+			.toggleClass( 'is-active' );
+
+		if ( $( this ).hasClass( 'is-active' ) ) {
+
+			$( '.menu-container' )
+				.removeClass( 'not-visible' )
+				.addClass( 'visible' );
+
+			$( 'body' ).css( 'overflow', 'hidden' );
+		} else {
+
+			// Remove the class 'visible' and add the class 'not-visible' class to the menu container.
+			$( '.menu-container' )
+				.removeClass( 'visible' )
+				.addClass( 'not-visible' );
+
+			$( 'body' ).css( 'overflow', 'auto' );
+		}
+
+	} );
+
+	$( '.menu-container .static-menu ul li a' ).on( 'mouseenter', function() {
+		var id = $( this ).data( 'id' );
+
+		if ( id === 'company' ) {
+			$( '.company' )
+				.removeClass( 'not-visible' )
+				.addClass( 'visible' );
+
+			$( '.projects' )
+				.removeClass( 'visible' )
+				.addClass( 'not-visible' );
+		} else if ( id === 'projects' ) {
+			$( '.projects' )
+				.removeClass( 'not-visible' )
+				.addClass( 'visible' );
+
+			$( '.company' )
+				.removeClass( 'visible' )
+				.addClass( 'not-visible' );
+		} else {
+			$( '.company' )
+				.removeClass( 'visible' )
+				.addClass( 'not-visible' );
+
+			$( '.projects' )
+				.removeClass( 'visible' )
+				.addClass( 'not-visible' );
+		}
+	} );
+
+
+
+
+
 		/**
 		 * When the hamburger button is clicked, toggle display of the menu.
 		 *
 		 * @since 0.1.0
 		 * @uses  toggleClass(), hasClass(), removeClass(), addClass()
 		 */
-		$( '.header-navigation-front-page .hamburger' ).on( 'click', function() {
-
-			// Toggle hamburger button appearance.
-			$( '.hamburger' )
-				.toggleClass( 'is-active' );
-
-			if ( $( this ).hasClass( 'is-active' ) ) {
-
-				// Remove the class 'not-visible' and add the class 'visible' class to the menu container.
-				$( '.menu-container' )
-					.removeClass( 'not-visible' )
-					.addClass( 'visible' );
-			} else {
-
-				// Remove the class 'visible' and add the class 'not-visible' class to the menu container.
-				$( '.menu-container' )
-					.removeClass( 'visible' )
-					.addClass( 'not-visible' );
-			}
-
-			// Make sure the second list element within menu container is not displayed by default.
-			$( '.menu-container ul:last-child' )
-				.removeClass( 'visible' );
-		});
+		// $( '.header-navigation-front-page .hamburger' ).on( 'click', function() {
+		//
+		// 	// Toggle hamburger button appearance.
+		// 	$( '.hamburger' )
+		// 		.toggleClass( 'is-active' );
+		//
+		// 	if ( $( this ).hasClass( 'is-active' ) ) {
+		//
+		// 		// Remove the class 'not-visible' and add the class 'visible' class to the menu container.
+		// 		$( '.menu-container' )
+		// 			.removeClass( 'not-visible' )
+		// 			.addClass( 'visible' );
+		// 	} else {
+		//
+		// 		// Remove the class 'visible' and add the class 'not-visible' class to the menu container.
+		// 		$( '.menu-container' )
+		// 			.removeClass( 'visible' )
+		// 			.addClass( 'not-visible' );
+		// 	}
+		//
+		// 	// Make sure the second list element within menu container is not displayed by default.
+		// 	$( '.menu-container ul:last-child' )
+		// 		.removeClass( 'visible' );
+		// });
 
 		/**
 		 * On hover display menu, display the other menu.
@@ -46,23 +105,23 @@
 		 * @since 0.1.0
 		 * @uses  toggleClass(), hasClass(), addClass(), removeClass()
 		 */
-		$( '.menu-container ul:first-child li a' ).on( 'mouseenter', function() {
-
-			// Define local variables.
-			var id = $( this ).data( 'id' );
-
-			if ( id == 'company' ) {
-
-				// Add class 'visible' to the menu
-				$( '.menu-container ul:last-child' )
-					.addClass( 'visible' );
-			} else {
-
-				// Remove calss 'visible' from the menu.
-				$( '.menu-container ul:last-child' )
-					.removeClass( 'visible' );
-			}
-		});
+		// $( '.menu-container ul:first-child li a' ).on( 'mouseenter', function() {
+		//
+		// 	// Define local variables.
+		// 	var id = $( this ).data( 'id' );
+		//
+		// 	if ( id == 'company' ) {
+		//
+		// 		// Add class 'visible' to the menu
+		// 		$( '.menu-container ul:last-child' )
+		// 			.addClass( 'visible' );
+		// 	} else {
+		//
+		// 		// Remove calss 'visible' from the menu.
+		// 		$( '.menu-container ul:last-child' )
+		// 			.removeClass( 'visible' );
+		// 	}
+		// });
 
 		/**
 		 * Dim the sub menus on hover of main menu items on the wall details template.
@@ -94,11 +153,11 @@
 		});
 
 		// Slick carousel controller
-		$('.carousel').slick({
+		$( '.carousel' ).slick({
 			slidesToShow: 1,
 			autoplay: true,
-  			autoplaySpeed: 3000,
-  			speed: 600,
+			autoplaySpeed: 3000,
+			speed: 600,
 		});
 
 	});
