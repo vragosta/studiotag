8c62d7071cd9c5ada06e77fdc44c2ca98835e772
Patrick Centeno <pcenteno3492@gmail.com>
2017-04-20 10:43:27 -0400

    Added social media links to footer

--- themes/twenty-seventeen/footer.php
+++ themes/twenty-seventeen/footer.php
@@ -46,11 +46,11 @@
 
 					<!-- Social Icons -->
 					<div class="social">
-						<a href="<?php echo esc_attr( $twitter ); ?>" class="twitter"><i class="fa fa-twitter fa-2x"></i></a>
-						<a href="<?php echo esc_attr( $facebook ); ?>" class="facebook"><i class="fa fa-facebook-official fa-2x"></i></a>
-						<a href="<?php echo esc_attr( $pinterest ); ?>" class="pinterest"><i class="fa fa-pinterest fa-2x"></i></a>
-						<a href="<?php echo esc_attr( $instagram ); ?>" class="instagram"><i class="fa fa-instagram fa-2x"></i></a>
-						<a href="<?php echo esc_attr( $linkedin ); ?>" class="linkedin"><i class="fa fa-linkedin-square fa-2x"></i></a>
+						<a href="https://twitter.com/tagwall_llc" class="twitter"><i class="fa fa-twitter fa-2x"></i></a>
+						<a href="https://www.facebook.com/studiotagnyc/" class="facebook"><i class="fa fa-facebook-official fa-2x"></i></a>
+						<a href="https://www.pinterest.com/tagwall/showroom/" class="pinterest"><i class="fa fa-pinterest fa-2x"></i></a>
+						<a href="https://www.instagram.com/studio_tag/" class="instagram"><i class="fa fa-instagram fa-2x"></i></a>
+						<a href="https://www.linkedin.com/company/studio-tag-llc" class="linkedin"><i class="fa fa-linkedin-square fa-2x"></i></a>
 					</div>
 
 				</div>
