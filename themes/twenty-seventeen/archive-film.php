19e35179174e62eb2cbf5fa766612a674708995b
Vincent Ragosta <allchenzo@gmail.com>
2017-04-21 15:00:26 -0400

    resolved bug in all grid templates

--- themes/twenty-seventeen/archive-film.php
+++ themes/twenty-seventeen/archive-film.php
@@ -109,7 +109,11 @@
 						<?php endwhile; ?>
 						<?php wp_reset_postdata(); ?>
 					</div>
-					<hr />
+
+					<?php if ( $taxonomy_query->post_count % 3 !== 0 ) : ?>
+						<hr />
+					<?php endif; ?>
+					
 				</div>
 			<?php endif; ?>
 
