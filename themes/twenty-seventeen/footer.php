1694c2c2060e0011ff3ac34c63c9bafc5b38208d
Vincent Ragosta <allchenzo@gmail.com>
2017-04-22 15:02:12 -0400

    widgetized left column of footer, modified css slightly for proportionality

--- themes/twenty-seventeen/footer.php
+++ themes/twenty-seventeen/footer.php
@@ -4,7 +4,7 @@
  *
  * @package Tag Wall - Twenty Seventeen
  * @since   0.1.0
- * @uses    get_user_by(), get_user_meta(), home_url(), esc_html(), esc_attr(), dynamic_sidebar(), wp_footer()
+ * @uses    TODO
  */
 ?>
 
@@ -16,26 +16,7 @@
 				</a>
 
 				<div>
-
-					<ul>
-						<li>321 West 44th Street</li>
-						<li>Suite 200</li>
-						<li>New York, NY 10036</li>
-					</ul>
-
-					<ul>
-						<li><a href="tel:<?php echo esc_attr( $phone ); ?>">+1 212.354.9255</a></li>
-						<li><a href="mailto:<?php echo esc_attr( $user->user_email ); ?>">info@studio-tag.com</a></li>
-					</ul>
-
-					<div class="social">
-						<a href="https://twitter.com/tagwall_llc" class="twitter"><i class="fa fa-twitter fa-2x"></i></a>
-						<a href="https://www.facebook.com/studiotagnyc/" class="facebook"><i class="fa fa-facebook-official fa-2x"></i></a>
-						<a href="https://www.pinterest.com/tagwall/showroom/" class="pinterest"><i class="fa fa-pinterest fa-2x"></i></a>
-						<a href="https://www.instagram.com/studio_tag/" class="instagram"><i class="fa fa-instagram fa-2x"></i></a>
-						<a href="https://www.linkedin.com/company/studio-tag-llc" class="linkedin"><i class="fa fa-linkedin-square fa-2x"></i></a>
-					</div>
-
+					<?php dynamic_sidebar( 'Footer Left Column' ); ?>
 				</div>
 
 			</section>
@@ -52,7 +33,7 @@
 						<h2>WREN</h2>
 					</div>
 				</div>
-				<hr style="width: 100%;" />
+				<hr />
 				<div class="row footer-menus">
 					<div class="col-xs-12 col-sm-4">
 						<?php dynamic_sidebar( 'Footer Column One' ); ?>
